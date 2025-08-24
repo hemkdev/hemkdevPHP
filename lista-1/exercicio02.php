6d250e24f4aa2be7bb3099160508d067d6b1a1ad
hemkdev <lucas.hemkemeier007@gmail.com>
2025-08-24 10:42:19 -0300

    att ex3

--- lista-1/exercicio02.php
+++ lista-1/exercicio02.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 02 - Cálculo de tabuada</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-   <form method="POST" action="">
+    <form method="POST" action="">
         <label for="numero">Digite um número:</label>
         <input type="number" id="numero" name="numero" required>
         <button type="submit" name="calcular_tabuada">Verificar</button>
     </form>
-    
+
     <?php
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         if (isset($_POST['calcular_tabuada'])) {
             $numero = filter_var($_POST['numero'], FILTER_VALIDATE_INT);
             echo '<div class="resultado">';
+            if ($numero === false) {
+                echo "Número inválido!";
+            }
             for ($i = 1; $i <= 10; $i++) {
                 $resultado = $numero * $i;
                 echo "$numero x $i = $resultado<br>";
@@ -27,4 +32,5 @@
     }
     ?>
 </body>
+
 </html>
--- lista-1/exercicio03.php
+++ lista-1/exercicio03.php
@@ -7,6 +7,32 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <form method="POST" action="">
+        <label for="numero">Digite um número:</label>
+        <input type="number" id="numero" name="numero" required>
+        <button type="submit" name="verificar_numero">Verificar</button>
+    </form>
 </body>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['verificar_numero'])) {
+            $numero = filter_var($_POST['numero'], FILTER_VALIDATE_INT);
+            echo '<div class="resultado">';
+            if ($numero === false) {
+                echo "Número inválido!";
+            } else {
+                if ($numero > 0) {
+                    echo "O número é positivo";
+                } else {
+                    if ($numero < 0) {
+                        echo "O número é negativo";
+                    } else {
+                        echo "O número é zero";
+                    }
+                }
+            }
+            echo '</div>';
+        }
+    }
+    ?> 
 </html>
