0c2a78f17e857bc135a62b9143f219947ace74e5
hemkdev <lucas.hemkemeier007@gmail.com>
2025-08-24 10:04:42 -0300

    add exercicios

---
+++ lista-1/exercicio13.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 13 - Conversão graus Celsius -> Fahrenheit </title>
+</head>
+<body>
+    
+</body>
+</html>
