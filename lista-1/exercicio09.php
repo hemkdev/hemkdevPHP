9598514db743b1411d126fa8f5996761b09d0593
hemkdev <lucas_l_hemkemeier@estudante.seisenai.org.br>
2025-08-25 09:33:16 -0300

    ex 09

--- lista-1/exercicio09.php
+++ lista-1/exercicio09.php
@@ -7,6 +7,42 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1> Exercício 09 - Soma de números entre variáveis </h1>
+    <form method="POST" action="">
+        <label for="num1">Digite o primeiro número:</label>
+        <input type="number" id="num1" name="num1" required>
+        <label for="num2">Digite o segundo número:</label>
+        <input type="number" id="num2" name="num2" required>
+        <button type="submit" name="somar_diferenca">Verificar</button>
+    </form>
+
+    <?php 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        $num1 = isset($_POST['num1']) ? (int)$_POST['num1'] : 0;
+        $num2 = isset($_POST['num2']) ? (int)$_POST['num2'] : 0;
+        echo '<div class="resultado">';
+
+          function somar_diferenca($num1, $num2) 
+          {
+              $soma = 0;
+              if ($num1 > $num2) {
+                  for ($i = $num2 + 1; $i < $num1; $i++) {
+                      $soma += $i;
+                  }
+              } else {
+                  for ($i = $num1 + 1; $i < $num2; $i++) {
+                      $soma += $i;
+                  }
+              }
+              return $soma;
+          }
+
+          $soma = somar_diferenca($num1, $num2);
+          echo "A soma dos números entre $num1 e $num2 é: $soma";
+
+        echo '</div>';
+     }
+    ?> 
 </body>
 </html>
