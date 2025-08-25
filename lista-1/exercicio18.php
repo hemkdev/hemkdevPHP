aeace6cfeb287837469c579fa9b9ffdf59ec6eca
hemkdev <lucas_l_hemkemeier@estudante.seisenai.org.br>
2025-08-25 09:54:42 -0300

    ex 18

--- lista-1/exercicio18.php
+++ lista-1/exercicio18.php
@@ -7,6 +7,37 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1> Exercício 18 - Maior número </h1>
+    <form method="POST" action="">
+        <label for="num1">Digite o primeiro número:</label>
+        <input type="number" id="num1" name="num1" required>
+        <label for="num2">Digite o segundo número:</label>
+        <input type="number" id="num2" name="num2" required>
+        <label for="num3">Digite o terceiro número:</label>
+        <input type="number" id="num3" name="num3" required>
+        <button type="submit" name="verificar_maior">Verificar</button>
+    </form>
+
+    <?php 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $num1 = isset($_POST['num1']) ? (int)$_POST['num1'] : 0;
+        $num2 = isset($_POST['num2']) ? (int)$_POST['num2'] : 0;
+        $num3 = isset($_POST['num3']) ? (int)$_POST['num3'] : 0;
+        echo '<div class="resultado">';
+
+        function verificar_maior($num1, $num2, $num3) {
+            if ($num1 >= $num2 && $num1 >= $num3) {
+                return $num1;
+            } elseif ($num2 >= $num1 && $num2 >= $num3) {
+                return $num2;
+            } else {
+                return $num3;
+            }
+        }
+        $maior_numero = verificar_maior($num1, $num2, $num3);
+        echo "O maior número entre $num1, $num2 e $num3 é: $maior_numero.";
+        echo '</div>';
+    }
+    ?>
 </body>
 </html>
