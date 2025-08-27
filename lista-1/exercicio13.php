70223fffd4f78bd326e9f6618dfa81339526c03d
hemkdev <lucas.hemkemeier007@gmail.com>
2025-08-27 19:40:59 -0300

    ex13

--- lista-1/exercicio13.php
+++ lista-1/exercicio13.php
@@ -7,6 +7,39 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1> Exercício 13 - Conversão Celsius -> Fahrenheit </h1>
+    <form method="POST" action="">
+        <label for="temperatura">Digite a temperatura</label>
+        <input type="number" id="temperatura" name="temperatura" required>
+        <button type="submit" name="conversao_celsius">Celsius -> Fahrenheit</button>
+        <button type="submit" name="conversao_fahrenheit">Fahrenheit -> Celsius</button>
+    </form>
+
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $temperatura = isset($_POST['temperatura']) ? floatval($_POST['temperatura']) : 0;
+        echo '<div class="resultado">';
+
+            function conversao_celsius($temperatura) {
+                $resultado = ($temperatura * (9 / 5)) + 32;
+                return round($resultado, 1);
+            }
+            function conversao_fahrenheit($temperatura) {
+                $resultado = ($temperatura - 32) * (5 / 9);
+                return round($resultado, 1);
+            }
+
+            if (isset($_POST['conversao_celsius'])) {
+                $conversao = conversao_celsius($temperatura);
+                echo "$temperatura &deg;C convertidos são $conversao &deg;F";
+            } elseif (isset($_POST['conversao_fahrenheit'])) {
+                $conversao = conversao_fahrenheit($temperatura);
+                echo "$temperatura &deg;F convertidos são $conversao &deg;C";
+            }
+            echo '</div>';
+
+        }
+    ?>
 </body>
+
 </html>
