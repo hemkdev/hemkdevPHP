027506bd483e721d7143370c04df8aedf43e9d3d
hemkdev <lucas.hemkemeier007@gmail.com>
2025-08-27 19:55:52 -0300

    ex 14

--- lista-1/exercicio14.php
+++ lista-1/exercicio14.php
@@ -7,6 +7,29 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <h1> Exercício 14 - Verificar ano bissexto </h1>
+    <form method="POST" action="">
+        <label for="ano">Digite o ano</label>
+        <input type="number" id="ano" name="ano" required>
+        <button type="submit" name="verificar_bissexto"> Verificar </button>
+    </form>
+
+    <?php 
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $ano = isset($_POST['ano']) ? (int)$_POST['ano'] : 0;
+        echo '<div class="resultado">';
+
+            function verificar_bissexto($ano) {
+                if ($ano % 4 === 0 && $ano % 100 != 0 || $ano % 400 === 0) {
+                    return $resultado = "O ano é bissexto";
+                } else {
+                    return $resultado = "O ano não é bissexto";
+                }
+            }
+            $resultado = verificar_bissexto($ano);
+            echo "$resultado";
+            echo "</div>";
+        }
+    ?>
 </body>
 </html>
