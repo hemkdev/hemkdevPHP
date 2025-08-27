05cc565dbae25d83d3797a948b2a1e5df74aa5c9
hemkdev <lucas.hemkemeier007@gmail.com>
2025-08-27 20:06:26 -0300

    terminar ex 17

--- lista-1/exercicio17.php
+++ lista-1/exercicio17.php
@@ -7,6 +7,12 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <h1> Exercício 17 - Verificar data válida </h1>
+    <form method="POST" action="">
+        <label for="data"> Digite a data </label>
+        <input type="date" id="data" name="data" required>
+        <button type="submit" name="verificar_data"> Verificar </button>
+    </form>
     
 </body>
 </html>
