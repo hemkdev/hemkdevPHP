7fc1cf7900c7e41a1a52c6d3b126652d19bb9580
hemkdev <lucas.hemkemeier007@gmail.com>
2025-08-28 19:47:30 -0300

    ex 17

--- lista-1/exercicio17.php
+++ lista-1/exercicio17.php
@@ -9,10 +9,30 @@
 <body>
     <h1> Exercício 17 - Verificar data válida </h1>
     <form method="POST" action="">
-        <label for="data"> Digite a data </label>
-        <input type="date" id="data" name="data" required>
+        <label for="data"> Digite a data DD/MM/AA </label>
+        <input type="number" id="dia" name="dia" required>
+        <input type="number" id="mes" name="mes" required>
+        <input type="number" id="ano" name="ano" required>
         <button type="submit" name="verificar_data"> Verificar </button>
     </form>
-    
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $ano = isset($_POST['ano']) ? (int)$_POST['ano'] : 0;
+        $mes = isset($_POST['mes']) ? (int)$_POST['mes'] : 0;
+        $dia = isset($_POST['dia']) ? (int)$_POST['dia'] : 0;
+        echo '<div class="resultado">';
+
+        function verificar_data($dia, $mes, $ano) {
+            if (checkdate($mes, $dia, $ano)) {
+                return $resultado = "A data $dia/$mes/$ano é válida";
+            } else {
+                return $resultado = "A data $dia/$mes/$ano não é válida";
+            }
+        }
+        $resultado = verificar_data($dia, $mes, $ano);
+        echo "$resultado";
+        echo "</div>";
+    }
+    ?>
 </body>
 </html>
